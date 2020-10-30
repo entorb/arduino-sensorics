22d08e3464aa0307753f75962637152543b0406e
Torben <59419684+entorb@users.noreply.github.com>
2020-10-30 18:00:20 +0100

    introducing Device Class, define device switches

--- libraries/TM/src/TM_InfluxDB.h
+++ libraries/TM/src/TM_InfluxDB.h
@@ -2,21 +2,24 @@
   TM_InfluxDB.h - Library for Connection to my InfluxDB
 */
 
-#ifndef TM_InfluxDB_H
-#define TM_InfluxDB_H
+#ifndef TM_INFLUXDB_H
+#define TM_INFLUXDB_H
+
+#include "TM_Device_Class.h"
 
 #include <InfluxDbClient.h>
 
-class TM_Influx_Class
+class TM_Influx_Class : public TM_Device_Class
 {
 public:
   // constructor
   TM_Influx_Class();
   // variables
   // functions
-  void connect_wifi(char *);
+  void connect_wifi(const char *);
   void connect_influxdb();
   void send_point(Point);
+  void sync_time();
 
 private:
   // InfluxDBClient my_InfluxClient;
