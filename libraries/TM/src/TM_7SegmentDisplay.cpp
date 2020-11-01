f19f51e46fcc3516860dd9c465a949383c081fa0
Torben <59419684+entorb@users.noreply.github.com>
2020-11-01 18:02:16 +0100

    proper use of clases, adding 7seg display

---
+++ libraries/TM/src/TM_7SegmentDisplay.cpp
@@ -0,0 +1,44 @@
+#include "TM_Device_Class.h"
+#include "TM_7SegmentDisplay_Class.h"
+
+#include <Arduino.h>
+#include <TM1637Display.h>
+
+// Module connection pins (Digital Pins)
+#define CLK 2
+#define DIO 3
+
+TM1637Display display(CLK, DIO); // TODO: my_display
+uint8_t data[4] = {0xff, 0xff, 0xff, 0xff};
+uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
+
+TM_7SegmentDisplay_Class::TM_7SegmentDisplay_Class() : TM_Device_Class()
+{
+}
+
+void TM_7SegmentDisplay_Class::init()
+{
+  if (verbose == true)
+  {
+    Serial.println(F("TM_7SegmentDisplay_Class init"));
+  }
+
+  display.clear();
+  display.setBrightness(0x0f);
+
+  // Selectively set different digits
+  data[0] = display.encodeDigit(0);
+  data[1] = display.encodeDigit(1);
+  data[2] = display.encodeDigit(2);
+  data[3] = display.encodeDigit(3);
+  display.setSegments(data);
+
+  display.showNumberDec(301, false); // Expect: _301
+  display.showNumberDec(301, true);  // Expect: 0301
+
+  for (int k = 0; k < 7; k++)
+  {
+    display.setBrightness(k);
+    display.setSegments(data);
+  }
+}
--- libraries/TM/src/TM_MH-Z19_Class.h
+++ libraries/TM/src/TM_MH-Z19_Class.h
@@ -6,12 +6,10 @@
 #define TM_MH_Z19_CLASS_H
 
 #include "TM_Device_Class.h"
-
 #include <Arduino.h>
 #include "MHZ19.h"
-#define RX_PIN 16           // Rx pin which the MHZ19 Tx pin is attached to
-#define TX_PIN 17           // Tx pin which the MHZ19 Rx pin is attached to
-#define MHZ19_BAUDRATE 9600 // Device to MH-Z19 Serial baudrate (should not be changed)
+
+
 
 class TM_MH_Z19_Class : public TM_Device_Class
 {
@@ -22,10 +20,10 @@ public:
   void init();
   int read_values();
   // variables
-  // TODO: Tut nicht :-(
-  // HardwareSerial mySerial_Class(1); // (ESP32 Example) create device to MH-Z19 serial
 
 private:
+  // variables
   MHZ19 myMHZ19;
+  HardwareSerial mySerial;
 };
 #endif
