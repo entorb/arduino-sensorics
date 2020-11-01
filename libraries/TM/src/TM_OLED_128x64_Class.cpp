ee6704650afbf2f48af8b705849f834f832e0127
Torben <59419684+entorb@users.noreply.github.com>
2020-11-01 20:04:44 +0100

    Adding OLED Master Class

--- libraries/TM/src/TM_OLED_128x64_Class.cpp
+++ libraries/TM/src/TM_OLED_128x64_Class.cpp
@@ -1,5 +1,5 @@
 /*
-  TM_OLED_128x32_Class.cpp - Library for Connection to 128x32 OLED Display
+  TM_OLED_128x64_Class.cpp - Library for Connection to 128x64 OLED Display
 */
 //based on
 /*
@@ -10,7 +10,7 @@
 
 #include "TM_Device_Class.h"
 #include "TM_OLED_Class.h"
-#include "TM_OLED_128x32_Class.h"
+#include "TM_OLED_128x64_Class.h"
 
 #include <Arduino.h>
 #include "U8g2lib.h"
@@ -22,7 +22,7 @@
 #endif
 
 // Initialisierungsliste
-TM_OLED_128x32_Class ::TM_OLED_128x32_Class() : TM_OLED_Class(), my_u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE)
+TM_OLED_128x64_Class ::TM_OLED_128x64_Class() : TM_OLED_Class(), my_u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE)
 {
   barchart_data[px_x] = {0}; // initialize with all with 0
 }
