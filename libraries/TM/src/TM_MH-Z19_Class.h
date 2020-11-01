20b980fcb7e643e52374e115fed31a8a9ae1c7d5
Torben <59419684+entorb@users.noreply.github.com>
2020-11-01 14:13:25 +0100

    experiments

--- libraries/TM/src/TM_MH-Z19_Class.h
+++ libraries/TM/src/TM_MH-Z19_Class.h
@@ -7,21 +7,25 @@
 
 #include "TM_Device_Class.h"
 
+#include <Arduino.h>
 #include "MHZ19.h"
+#define RX_PIN 16           // Rx pin which the MHZ19 Tx pin is attached to
+#define TX_PIN 17           // Tx pin which the MHZ19 Rx pin is attached to
+#define MHZ19_BAUDRATE 9600 // Device to MH-Z19 Serial baudrate (should not be changed)
 
 class TM_MH_Z19_Class : public TM_Device_Class
 {
 public:
   // constructor
-  // HardwareSerial mySerial_Class(1); // (ESP32 Example) create device to MH-Z19 serial
   TM_MH_Z19_Class();
   // functions
   void init();
   int read_values();
   // variables
+  // TODO: Tut nicht :-(
+  // HardwareSerial mySerial_Class(1); // (ESP32 Example) create device to MH-Z19 serial
 
 private:
   MHZ19 myMHZ19;
-  // TODO: HardwareSerial mySerial_Class;
 };
 #endif
--- libraries/TM/src/TM_OLED_128x32_Class.h
+++ libraries/TM/src/TM_OLED_128x32_Class.h
@@ -7,6 +7,16 @@
 
 #include "TM_Device_Class.h"
 
+#include <Arduino.h>
+
+#include <U8g2lib.h>
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
 class TM_OLED_128x32_Class : public TM_Device_Class
 {
 public:
@@ -19,8 +29,12 @@ public:
   void drawBarchart(const float);
   void drawAltBarchartOrInt(const float);
   void setBarchartRange(const float, const float);
+  //variables
+  // static U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C my_u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+  // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
 
 private:
+  //variables
   const static unsigned int px_x = 128;
   const static unsigned int px_y = 32;
   unsigned int barchart_min;
