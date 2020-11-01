0c736b2223cab8c52e95c0d734429fd1dcfc83d4
Torben <59419684+entorb@users.noreply.github.com>
2020-11-01 20:40:27 +0100

    bugfix

--- 0_arduino_code/device_setup.h
+++ 0_arduino_code/device_setup.h
@@ -1,13 +1,13 @@
 
 // setup of this device
-static const int mySleep = 60 * 1000; // milli sec
+static const int mySleep = 2 * 1000; // milli sec
 // deprecated, because not available in classes: #define TM_VERBOSE 1 // 0 disables all Serial.print() calls
 static const bool myVerbose = false;
-#define TM_LOAD_DEVICE_INFLUXDB 1
+#define TM_LOAD_DEVICE_INFLUXDB 0
 #define TM_LOAD_DEVICE_BME280 0
 #define TM_LOAD_DEVICE_MHZ19 1
 #define TM_LOAD_DEVICE_OLED_128X32 0
-#define TM_LOAD_DEVICE_OLED_128X64 0
+#define TM_LOAD_DEVICE_OLED_128X64 1
 static const char my_device_name[] = "T-ESP32-1";
 static const char my_room[] = "Arbeitszimmer";
 /*
--- libraries/TM/src/TM_OLED_128x32_Class.h
+++ libraries/TM/src/TM_OLED_128x32_Class.h
@@ -26,9 +26,6 @@ public:
   TM_OLED_128x32_Class();
   // functions
   // variables
-  const unsigned int px_x = 128;
-  const unsigned int px_y = 32;
-  unsigned int barchart_data[128];
 
 private:
   //variables
