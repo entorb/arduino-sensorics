9d2f131a13729ea60b33f10eae5af9f0163dfa74
Torben <59419684+entorb@users.noreply.github.com>
2020-11-01 14:13:25 +0100

    experiments

--- libraries/TM/src/TM_MH-Z19_Class.cpp
+++ libraries/TM/src/TM_MH-Z19_Class.cpp
@@ -8,12 +8,11 @@
 
 #include <Arduino.h>
 #include "MHZ19.h"
-
 #define RX_PIN 16           // Rx pin which the MHZ19 Tx pin is attached to
 #define TX_PIN 17           // Tx pin which the MHZ19 Rx pin is attached to
 #define MHZ19_BAUDRATE 9600 // Device to MH-Z19 Serial baudrate (should not be changed)
 
-// TODO: move into class, but this led to reboots of the Arduino, so research needed
+// TODO: move into class: // TODO: Tut nicht :-(
 HardwareSerial mySerial_Class(1); // (ESP32 Example) create device to MH-Z19 serial
 
 TM_MH_Z19_Class::TM_MH_Z19_Class() : TM_Device_Class()
@@ -26,7 +25,8 @@ void TM_MH_Z19_Class::init()
   {
     Serial.println(F("MH-Z19 init"));
   }
-  // TODO: mySerial_Class(1);                                                // TODO: Tut nicht
+  // TODO: Tut nicht :-(
+  // mySerial_Class(1);                                                // (ESP32 Example) create device to MH-Z19 serial
   mySerial_Class.begin(MHZ19_BAUDRATE, SERIAL_8N1, RX_PIN, TX_PIN); // (ESP32 Example) device to MH-Z19 serial start
   myMHZ19.begin(mySerial_Class);                                    // *Serial(Stream) reference must be passed to library begin().
 
