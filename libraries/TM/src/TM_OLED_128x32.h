27a4ea5c83e786bb8a24ffc5716ccd283adf8e2b
Torben <59419684+entorb@users.noreply.github.com>
2020-11-01 12:17:13 +0100

    added barchart

--- libraries/TM/src/TM_OLED_128x32.h
+++ libraries/TM/src/TM_OLED_128x32.h
@@ -14,12 +14,19 @@ public:
   TM_OLED_128x32_Class();
   // functions
   void init();
-  void drawStr(const int, const int, const char *);
+  void drawStr(const char *);
+  void drawInt(const unsigned int);
+  void drawBarchart(const float);
+  void drawAltBarchartOrInt(const float);
+  void setBarchartRange(const float, const float);
 
 private:
-  int barchart_min;
-  int barchart_max;
-  int barchart_line;
+  const static unsigned int px_x = 128;
+  const static unsigned int px_y = 32;
+  unsigned int barchart_min;
+  unsigned int barchart_max;
+  unsigned int barchart_data[px_x]; //TODO: is there a way of defining the array length upon creation of object from class
+  bool last_was_barchart = false;   // alternate between bar and int chart
 };
 
 #endif
