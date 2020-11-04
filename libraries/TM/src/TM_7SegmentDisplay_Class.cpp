1a9809daeb85773b5040105722ccfa9be61e985a
Torben <59419684+entorb@users.noreply.github.com>
2020-11-04 21:41:43 +0100

    added Display Device Class

--- libraries/TM/src/TM_7SegmentDisplay_Class.cpp
+++ libraries/TM/src/TM_7SegmentDisplay_Class.cpp
@@ -6,49 +6,19 @@ Class for accessing 4 x 7 segment clock display
 
 #include "TM_Device_Class.h"
 #include "TM_7SegmentDisplay_Class.h"
+#include "TM_Helper.h"
 
 #include <TM1637Display.h>
 
-// Module connection pins (Digital Pins)
-// #define CLK 26
-// #define DIO 25
-
-TM_7SegmentDisplay_Class::TM_7SegmentDisplay_Class(const uint8_t pin_clk, const uint8_t pin_dio, const bool v) : TM_Device_Class(v), myDisplay(pin_clk, pin_dio)
+TM_7SegmentDisplay_Class::TM_7SegmentDisplay_Class(const uint8_t pin_clk, const uint8_t pin_dio, const bool v) : TM_Display_Device_Class(v), myDisplay(pin_clk, pin_dio)
 {
   myDisplay.clear();
   myDisplay.setBrightness(0x0f);
-}
-
-void TM_7SegmentDisplay_Class::test()
-{
-  myDisplay.clear();
-  myDisplay.setBrightness(0x0f);
-
-  // Selectively set different digits
-  // data[0] = myDisplay.encodeDigit(0);
-  // data[1] = myDisplay.encodeDigit(1);
-  // data[2] = myDisplay.encodeDigit(2);
-  // data[3] = myDisplay.encodeDigit(3);
-  // myDisplay.setSegments(data);
-  // delay(1000);
-
-  // myDisplay.showNumberDec(301, false); // Expect: _301
-  // myDisplay.showNumberDec(301, true); // Expect: 0301
-
-  myDisplay.showNumberDec(1234, false);
-
-  for (int k = 0; k < 7; k++)
-  {
-    Serial.println(k);
-    myDisplay.setBrightness(k);
-    myDisplay.setSegments(data);
-    delay(1000);
-  }
 }
 
 void TM_7SegmentDisplay_Class::setBrightness(uint8_t brightness)
 {
-  // 0 -> off, 7 -> max
+  // 0 -> min, 7 -> max
   myDisplay.setBrightness(brightness);
 }
 
@@ -59,4 +29,10 @@ void TM_7SegmentDisplay_Class::displayValue(uint16_t value)
   myDisplay.showNumberDec(value, false);
 }
 
-//TODO: calc brightness based on value, min, max
+void TM_7SegmentDisplay_Class::displayValueAndSetBrightness(uint16_t value)
+{
+  uint8_t brightnessLevel = tm_helper_value_to_category(value, value_min, value_max, _num_brightness_levels);
+
+  setBrightness(brightnessLevel);
+  displayValue(value);
+}
--- libraries/TM/src/TM_Device_Class.cpp
+++ libraries/TM/src/TM_Device_Class.cpp
@@ -5,16 +5,25 @@ Abstract Class that all other device classes inherit from
 #include "Arduino.h"
 #include "TM_Device_Class.h"
 
-TM_Device_Class::TM_Device_Class(const bool v)
+TM_Device_Class::TM_Device_Class(const bool this_verbose)
 {
-  verbose = v;
+  verbose = this_verbose;
 }
 
-void TM_Device_Class::setVerbose(bool v)
+void TM_Device_Class::setVerbose(bool this_verbose)
 {
-  verbose = v;
+  verbose = this_verbose;
 }
 
 void TM_Device_Class::init()
 {
+}
+
+// this is a sub-class of Device_Class, that additionally stores value_min and value_max as display scale range
+TM_Display_Device_Class::TM_Display_Device_Class(const bool v) : TM_Device_Class(v){};
+
+void TM_Display_Device_Class::setValueRange(const float this_value_min, const float this_value_max)
+{
+  value_min = value_min;
+  value_max = value_max;
 }
