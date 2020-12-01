d815e8098c57b371ce46fc84ccd00b9e2d15c62b
Torben <59419684+entorb@users.noreply.github.com>
2020-12-01 08:24:23 +0100

    KiTa V2

--- 0_arduino_code/device_setup.h
+++ 0_arduino_code/device_setup.h
@@ -13,28 +13,40 @@ static const bool myVerbose = false;
 // static const int mySleep = 2 * 1000;
 // static const bool myVerbose = true;
 
+//
 // Devices to load
-#define TM_LOAD_DEVICE_INFLUXDB
+//
+// #define TM_LOAD_DEVICE_INFLUXDB
 // #define TM_HOUR_SLEEP 22
 // #define TM_HOUR_WAKE 6
 // #define TM_LOAD_DEVICE_BME280
-#define TM_LOAD_DEVICE_BME680
-#define TM_LOAD_DEVICE_BH1750
+// #define TM_LOAD_DEVICE_BME680
+// #define TM_LOAD_DEVICE_BH1750
+
 #define TM_LOAD_DEVICE_MHZ19
+// Old KiTa Layout
+#define TM_MHZ19_PIN_RX 14
+#define TM_MHZ19_PIN_TX 13
+// Newer layout
+// #define TM_MHZ19_PIN_RX 16
+// #define TM_MHZ19_PIN_TX 17
+
 #define TM_LOAD_DEVICE_4_DIGIT
-#define TM_LOAD_DEVICE_OLED_128X32
+// #define TM_LOAD_DEVICE_OLED_128X32
 // #define TM_LOAD_DEVICE_OLED_128X64
-// #define TM_LOAD_DEVICE_LED_KY_016
+#define TM_LOAD_DEVICE_LED_KY_016
 // #define TM_LOAD_DEVICE_LED_RING
 
+//
 // EPS32 Setup
-
-static const char my_device_name[] = "T-ESP32-1";
-static const char my_room[] = "Arbeitszimmer";
+//
+// static const char my_device_name[] = "T-ESP32-1";
+// static const char my_room[] = "Arbeitszimmer";
 // static const char my_device_name[] = "T-ESP32-2"; static const char my_room[] = "Bad";
 // static const char my_device_name[] = "T-ESP32-3"; static const char my_room[] = "Kind 1";
 // static const char my_device_name[] = "T-ESP32-4"; static const char my_room[] = "Wohnzimmer";
 // static const char my_device_name[] = "T-ESP32-5"; static const char my_room[] = "Wintergarten";
 // static const char my_device_name[] = "T-ESP32-6"; static const char my_room[] = "Wohnzimmer Falls";
-
 // static const char my_device_name[] = "T-ESP32-x"; static const char my_room[] = "ASGS-1A";
+static const char my_device_name[] = "T-ESP32-x";
+static const char my_room[] = "KiTa";
