b67e03430637b598d9ead2aeeb21904054d3f219
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-01-27 22:58:04 +0100

    deployment

--- 0_arduino_code/device_setup.h
+++ 0_arduino_code/device_setup.h
@@ -6,23 +6,23 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_ERROR
 
 // Normal Mode
-static const int mySleep = 6 * 1000;
+static const int mySleep = 5 * 1000;
 // static const int mySleep = 3 * 1000; // for BME680 in LP mode it must be 3s!
 // static const bool myVerbose = false;
 
 // Debugging
 //  static const int mySleep = 2 * 1000;
- static const bool myVerbose = true;
+ static const bool myVerbose = false;
 
 //
 // Devices to load
 //
-#define TM_LOAD_DEVICE_INFLUXDB
+// #define TM_LOAD_DEVICE_INFLUXDB
 // #define TM_HOUR_SLEEP 22
 // #define TM_HOUR_WAKE 6
 #define TM_LOAD_DEVICE_BME280
-// #define TM_LOAD_DEVICE_BME680
 // #define TM_LOAD_DEVICE_BH1750
+// #define TM_LOAD_DEVICE_BME680
 
 #define TM_LOAD_DEVICE_MHZ19
 // Newer layout
@@ -33,10 +33,10 @@ static const int mySleep = 6 * 1000;
 // #define TM_MHZ19_PIN_TX 13
 
 // Displays
-// #define TM_LOAD_DEVICE_4_DIGIT
-// #define TM_LOAD_DEVICE_LED_KY_016
+#define TM_LOAD_DEVICE_4_DIGIT
+#define TM_LOAD_DEVICE_LED_KY_016
 // #define TM_LOAD_DEVICE_OLED_128X64
-// #define TM_LOAD_DEVICE_OLED_128X32
+#define TM_LOAD_DEVICE_OLED_128X32
 // #define TM_LOAD_DEVICE_LED_RING
 
 //
@@ -46,14 +46,14 @@ static const int mySleep = 6 * 1000;
 // static const char my_device_name[] = "T-ESP32-2"; static const char my_room[] = "Bad";
 // static const char my_device_name[] = "T-ESP32-3"; static const char my_room[] = "Eleen";
 // static const char my_device_name[] = "T-ESP32-4"; static const char my_room[] = "Wohnzimmer";
-static const char my_device_name[] = "T-ESP32-5"; static const char my_room[] = "Wintergarten";
+// static const char my_device_name[] = "T-ESP32-5"; static const char my_room[] = "Wintergarten";
 // static const char my_device_name[] = "T-ESP32-6"; static const char my_room[] = "Wohnzimmer Falls";
 // static const char my_device_name[] = "T-ESP32-7"; static const char my_room[] = "Hanno";
 // static const char my_device_name[] = "T-ESP32-7"; static const char my_room[] = "Wohnzimmer";
 // static const char my_device_name[] = "T-ESP32-8"; static const char my_room[] = "Hanno";
-// static const char my_device_name[] = "T-ESP32-9"; static const char my_room[] = "mobil";
+// static const char my_device_name[] = "T-ESP32-9"; static const char my_room[] = "Eleen";
 
-// static const char my_device_name[] = "T-ESP32-x";
+static const char my_device_name[] = "T-ESP32-x"; static const char my_room[] = "mobil";
 // static const char my_room[] = "ASGS-1A";
 // static const char my_room[] = "KiTa";
 // static const char my_room[] = "Wohnzimmer Menkes";
