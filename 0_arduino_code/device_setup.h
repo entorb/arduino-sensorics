5c164a6954505d77b1f17753d7bf7d24334c7ba6
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-04-25 08:26:42 +0200

    added 0_MH-Z19-Recovery

--- 0_arduino_code/device_setup.h
+++ 0_arduino_code/device_setup.h
@@ -12,7 +12,7 @@ static const int mySleep = 5 * 1000;
 
 // Debugging
 //  static const int mySleep = 2 * 1000;
- static const bool myVerbose = true;
+static const bool myVerbose = false;
 
 //
 // Devices to load
@@ -20,7 +20,7 @@ static const int mySleep = 5 * 1000;
 // #define TM_LOAD_DEVICE_INFLUXDB
 // #define TM_HOUR_SLEEP 22
 // #define TM_HOUR_WAKE 6
-#define TM_LOAD_DEVICE_BME280
+// #define TM_LOAD_DEVICE_BME280
 // #define TM_LOAD_DEVICE_BH1750
 // #define TM_LOAD_DEVICE_BME680
 
@@ -37,24 +37,7 @@ static const int mySleep = 5 * 1000;
 #define TM_LOAD_DEVICE_4_DIGIT
 #define TM_LOAD_DEVICE_LED_KY_016
 // #define TM_LOAD_DEVICE_OLED_128X64
-#define TM_LOAD_DEVICE_OLED_128X32
+// #define TM_LOAD_DEVICE_OLED_128X32
 // #define TM_LOAD_DEVICE_LED_RING
 
-//
-// EPS32 Setup
-//
-// static const char my_device_name[] = "T-ESP32-1"; static const char my_room[] = "Arbeitszimmer";
-// static const char my_device_name[] = "T-ESP32-2"; static const char my_room[] = "Bad";
-// static const char my_device_name[] = "T-ESP32-3"; static const char my_room[] = "Eleen";
-// static const char my_device_name[] = "T-ESP32-4"; static const char my_room[] = "Wohnzimmer";
-// static const char my_device_name[] = "T-ESP32-5"; static const char my_room[] = "Wintergarten";
-// static const char my_device_name[] = "T-ESP32-6"; static const char my_room[] = "Wohnzimmer Falls";
-// static const char my_device_name[] = "T-ESP32-7"; static const char my_room[] = "Hanno";
-// static const char my_device_name[] = "T-ESP32-7"; static const char my_room[] = "Wohnzimmer";
-// static const char my_device_name[] = "T-ESP32-8"; static const char my_room[] = "Hanno";
-// static const char my_device_name[] = "T-ESP32-9"; static const char my_room[] = "Eleen";
-
 static const char my_device_name[] = "T-ESP32-x"; static const char my_room[] = "mobil";
-// static const char my_room[] = "ASGS-1A";
-// static const char my_room[] = "KiTa";
-// static const char my_room[] = "Wohnzimmer Menkes";
