e72d93ec292c21bea8bf5db1d80451b8ddcdaffb
Torben Menke <59419684+entorb@users.noreply.github.com>
2021-10-02 10:29:56 +0200

    cleanup

--- 0_arduino_code/device_setup.h
+++ 0_arduino_code/device_setup.h
@@ -1,38 +1,23 @@
-//
-// setup this device
-//
+// General
+static const char my_device_name[] = "T-ESP32-1";
+static const char my_room[] = "Arbeitszimmer";
 
-// see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/log.html
-// #define LOG_LOCAL_LEVEL ESP_LOG_ERROR
-
-// Normal Mode
-static const int mySleep = 6 * 1000; // = time of loop
-// static const int mySleep = 3 * 1000; // for BME680 in LP mode it must be 3s!
+static const int mySleep = 5 * 1000; // = duration of loop
 static const bool myVerbose = false;
 
-// Debugging
-// static const int mySleep = 2 * 1000;
-// static const bool myVerbose = true;
-
-//
-// Devices to load
-//
+// Upload to Influx DB and clock settings
 #define TM_LOAD_DEVICE_INFLUXDB
+#define TM_DISPLAY_TIME
 #define TM_HOUR_SLEEP 21
 #define TM_HOUR_WAKE 7
-#define TM_DISPLAY_TIME
+
+// Sensors
 #define TM_LOAD_DEVICE_BME280
 #define TM_LOAD_DEVICE_BH1750
 // #define TM_LOAD_DEVICE_BME680
-
-// Sensors
 #define TM_LOAD_DEVICE_MHZ19
-// Newer layout
 #define TM_MHZ19_PIN_RX 16
 #define TM_MHZ19_PIN_TX 17
-// Old KiTa Layout
-// #define TM_MHZ19_PIN_RX 14
-// #define TM_MHZ19_PIN_TX 13
 
 // Displays
 #define TM_LOAD_DEVICE_4_DIGIT
@@ -40,6 +25,3 @@ static const bool myVerbose = false;
 // #define TM_LOAD_DEVICE_OLED_128X64
 #define TM_LOAD_DEVICE_OLED_128X32
 // #define TM_LOAD_DEVICE_LED_RING
-
-static const char my_device_name[] = "T-ESP32-1";
-static const char my_room[] = "Arbeitszimmer";
